be9e4081f55dfa82024df46a8abcef7ca778342f
Chris KH Han <chris.kh.han@gmail.com>
2020-11-09 04:16:16 -0600

    add Home.js

---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function Home() {
+    return (     
+        <div className="white f1 mt7">
+            {'Welcome to Face Recognition App'}
+        </div>
+    )
+}
+
+export default Home
