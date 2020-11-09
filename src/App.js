2a7688b7cf7b4997e7cf56deca13684706c1756b
Chris KH Han <chris.kh.han@gmail.com>
2020-11-09 04:17:02 -0600

    add react-router features on App.js and Navigation.js

--- src/App.js
+++ src/App.js
@@ -1,64 +1,85 @@
-import React, { useState } from 'react'
-import Clarifai from 'clarifai'
+import React, { useState } from "react";
+import { Switch, Route } from "react-router-dom";
+import Clarifai from "clarifai";
 
-import Navigation from './components/Navigation/Navigation'
-import Logo from './components/Logo/Logo'
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Home from "./components/Home/Home";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 // import Rank from './components/Rank/Rank'
-import FaceRecognition from './components/FaceRecognition/FaceRecognition'
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+import SignIn from "./components/SignIn/SignIn";
+import Register from "./components/Register/Register";
 
 const API_KEY = process.env.REACT_APP_FACE_RECOGNITION_API_KEY;
 
 const app = new Clarifai.App({
-  apiKey: `${API_KEY}`
- });
+  apiKey: `${API_KEY}`,
+});
 
 function App() {
-
-  const [input, setInput] = useState('')
-  const [imageUrl, setImageUrl] = useState('')
-  const [faceBox, setFaceBox] = useState({})
-
+  const [input, setInput] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [faceBox, setFaceBox] = useState({});
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  console.log(isSignedIn)
   const inputChange = (e) => {
-    setInput(e.target.value)
-  }
+    setInput(e.target.value);
+  };
 
   const submitImg = () => {
-      app.models.initModel({id: Clarifai.FACE_DETECT_MODEL,})
-      .then(faceDetectModel => {
+    app.models
+      .initModel({ id: Clarifai.FACE_DETECT_MODEL })
+      .then((faceDetectModel) => {
         return faceDetectModel.predict(input);
       })
-      .then(response => {
-        const faceBoxInImg = response.outputs[0].data.regions[0].region_info.bounding_box
-        calculateFaceBox(faceBoxInImg)
-      
-      })
+      .then((response) => {
+        const faceBoxInImg =
+          response.outputs[0].data.regions[0].region_info.bounding_box;
+        calculateFaceBox(faceBoxInImg);
+      });
 
-      setImageUrl(input)
-  }
+    setImageUrl(input);
+  };
 
   const calculateFaceBox = (imgBoxData) => {
-
-    const img = document.getElementById('img')
+    const img = document.getElementById("img");
     const width = Number(img.width);
     const height = Number(img.height);
     setFaceBox({
       topRow: height * imgBoxData.top_row,
-      bottomRow: height - (height * imgBoxData.bottom_row),
+      bottomRow: height - height * imgBoxData.bottom_row,
       leftCol: width * imgBoxData.left_col,
-      rightCol: width - (width * imgBoxData.right_col),
-    })
+      rightCol: width - width * imgBoxData.right_col,
+    });
+  };
+
+  const HasSignedIn = () => {
+    setIsSignedIn(prevBool => prevBool = ! prevBool)
   }
 
   return (
     <div className="tc">
-      <Navigation />
-      <Logo />
-      {/* <Rank /> */}
-      <ImageLinkForm inputChange={inputChange} submitImg={submitImg}/>
-      <FaceRecognition faceBox={faceBox} imgSrc={imageUrl}/>
+      <Navigation isSignedIn={isSignedIn} HasSignedIn={HasSignedIn} />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/sign-in">
+          <SignIn HasSignedIn={HasSignedIn} />
+        </Route>
+        <Route path="/register">
+          <Register HasSignedIn={HasSignedIn} />
+        </Route>
+        <Route path="/app">
+          <Logo />
+          {/* <Rank /> */}
+          <ImageLinkForm inputChange={inputChange} submitImg={submitImg} />
+          <FaceRecognition faceBox={faceBox} imgSrc={imageUrl} />
+        </Route>
+      </Switch>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-function Navigation() {
-    return (
-        <nav className="flex justify-end">
-            <p className="f3 link dim black underline pa3 pointer tr">Sign out</p>
-        </nav>
-    )
+
+
+function Navigation({ isSignedIn, HasSignedIn }) {
+
+  return (
+    <nav className="flex justify-end">
+      {!isSignedIn ? (
+        <div>
+          <Link to="/sign-in">
+            <span className="dib f3 link dim black underline pa3 pointer tr no-underline">
+              Sign In
+            </span>
+         </Link>
+          <Link to="/register">
+            <span className="dib f3 link dim black underline pa3 pointer tr no-underline">
+              Register
+            </span>
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <Link to="/">
+            <span 
+                className="dib f3 link dim black underline pa3 pointer tr no-underline"
+                onClick={HasSignedIn}
+            >
+              Sign out
+            </span>
+          </Link>
+        </div>
+      )}
+    </nav>
+  );
 }
 
-export default Navigation
+export default Navigation;
